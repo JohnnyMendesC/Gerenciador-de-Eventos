4b004182ec7ebf8185ac8fb6390ad3a4e596c58e
JohnnyMendesC <johnnymendescarmo@gmail.com>
2025-01-07 18:54:14 -0300

    Primeira atualização do Projeto Gerenciador de Eventos - Organização de pastas e Criação dos projetos em seus respectivos frameworks: SpringBoot, React e React-Native

---
+++ gerenciador-de-eventos-backend-spring/src/main/java/com/desafio/neki/GerenciadorDeEventosBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.neki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeEventosBackendSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeEventosBackendSpringApplication.class, args);
+	}
+
+}
